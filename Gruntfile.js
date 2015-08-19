895d2d0f01dc228ffcd7ca8b5c4a8e6fabaa6b4e
Scott Wu <scott.wu.hjx@gmail.com>
2015-08-19 00:52:00 -0400

    Added slider ball and followcircle

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,23 @@
-module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg : grunt.file.readJSON("package.json"),
-		
-		concat : {
-			options : {
-				sourceMap : true	
-			},
-			build : {
-				src : [
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+
+        concat: {
+            options: {
+                sourceMap: true
+            },
+            build: {
+                src: [
 					//""lib/**/*.js"
 					"lib/WOsuUtil.js",
-					
+
 					"lib/beatmap/Beatmap.js",
 					"lib/beatmap/BeatmapLoader.js",
 					"lib/beatmap/BeatmapData.js",
 					"lib/beatmap/BeatmapEvents.js",
 					"lib/beatmap/BeatmapObjects.js",
 					"lib/beatmap/BeatmapMechanics.js",
-                    
+
 					"lib/event/Event.js",
 					"lib/event/BackgroundEvent.js",
 					"lib/event/BreakPeriod.js",
@@ -25,32 +25,34 @@ module.exports = function(grunt) {
 					"lib/event/StoryboardObject.js",
 					"lib/event/TimingPoint.js",
 					"lib/event/ColourEvent.js",
-                    
+
 					"lib/event/GameEvent.js",
-					
+
 					"lib/object/HitObject.js",
 					"lib/object/CircleObject.js",
 					"lib/object/SliderObject.js",
 					"lib/object/SpinnerObject.js",
-					
+
 					"lib/player/Player.js",
 					"lib/player/ThreePlayer.js",
 					"lib/player/ScoreManager.js",
-					
+
 					"lib/math/Matrix3.js",
 					"lib/replay/Replay.js",
 					"lib/replay/ReplayLoader.js",
-					
+
 					"lib/skin/Skin.js",
 					"lib/skin/SkinLoader.js"
 				],
-				dest : "ext/WOsu.js"
-			}
-		}
-	});
-	
-	var tasks = [ "concat" ];
-	tasks.map(function(t) { grunt.loadNpmTasks("grunt-contrib-" + t); });
-	
-	grunt.registerTask("default", tasks);
+                dest: "ext/WOsu.js"
+            }
+        }
+    });
+
+    var tasks = ["concat"];
+    tasks.map(function (t) {
+        grunt.loadNpmTasks("grunt-contrib-" + t);
+    });
+
+    grunt.registerTask("default", tasks);
 }
